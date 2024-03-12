e6bc92bd255d75415f7fa6dba9a2c491a99b59e5
Cory Chiang <cory.scorpio@gmail.com>
2024-03-12 19:59:41 +0800

    dockerenv.sh for convenience

---
+++ dockerenv.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo_command() {
+    echo "Usage: [sudo] ${0} {command} [parameter ...]"
+    echo "  "
+    echo "    <command>"
+    echo "      setup                     Setup build environment image"
+    echo "      new [container_name]      Create new build working container"
+    echo "      resume [container_name]   Resume a previous created build working container"
+}
+
+if [ $# -lt 1 ]; then
+    echo_command
+    exit 0
+fi
+
+if [ "$1" = "setup" ]; then
+    docker build -t np_buildenv -f docker/buildenv.dockerfile . --no-cache
+
+elif [ "$1" = "new" ]; then
+    if [ $# -lt 2 ]; then
+        docker run --name np_practice -v $(pwd):/root/host -it np_buildenv bash
+    else
+        docker run --name $2 -v $(pwd):/root/host -it iii5gc_buildenv bash
+    fi
+
+elif [ "$1" = "resume" ]; then
+    if [ $# -lt 2 ]; then
+        docker start -i np_practice
+    else
+        docker start -i $2
+    fi
+
+else
+    echo_command
+    exit 0
+fi
